34295ec93238c5fcf2e0b73c54287292f10737ed
Your Name <you@example.com>
2024-07-22 06:41:39 -0700

    Add

--- scripts/fai/faime.sh
+++ scripts/fai/faime.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-curl "https://fai-project.org/cgi/faime.cgi?type=install;username=debian;gittype=user;gituser=https%3A%2F%2Fgithub.com%2Fqtvhao.keys;partition=ONE;keyboard=us;suite=bookworm;rclocal=1;sbm=2"
+curl "https://fai-project.org/cgi/faime.cgi?type=install;username=debian;gittype=github;gituser=https%3A%2F%2Fgithub.com%2Fqtvhao.keys;partition=ONE;keyboard=us;suite=bookworm;cl5=SSH_SERVER;cl7=NONFREE;sbm=2"
+
